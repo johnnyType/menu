6269c93f49716aac2169cb0a1a612cb77c68de7b
Yangjunli <asumaster.yangjun@gmail.com>
2021-03-06 16:36:15 -0800

    test file amend and refine

--- generate_menu.py
+++ generate_menu.py
@@ -2,9 +2,18 @@ from utils.date_to_number import date_to_number
 from utils.num_to_binary_list import num_to_binary_list
 from utils.filter_list import filter_list
 
+def get_topping_list(): ## topping list is one source of truth which will be reused also in test
+     ## topping list orders as three non-vegan toppings as the first 3 elements, the rest 9 topping are all vegan
+    return ["ham","bacon","pepperoni","garlic","mushroom","pineapple","green peppers","arugula","basil","onion","olives","artichoke"]
+
+def get_non_vegan_topping_list():
+    return ["ham","bacon","pepperoni"]
+
+def get_vegan_topping_list():
+    return ["garlic","mushroom","pineapple","green peppers","arugula","basil","onion","olives","artichoke"]
+
 def main(date):
-    ## topping list orders as three non-vegan toppings as the first 3 elements, the rest 9 topping are all vegan
-    topping_list = ["ham","bacon","pepperoni","garlic","mushroom","pineapple","green peppers","arugula","basil","onion","olives","artichoke"]
+    topping_list = get_topping_list()
     ## map today date to a fixed number based on month and day, the num range is from 1 to 372
     num = date_to_number(date)
     menu = []
--- test_menu_generate.py
+++ test_menu_generate.py
@@ -1,6 +1,17 @@
 import datetime
-from generate_menu import main
+from generate_menu import main, get_topping_list, get_non_vegan_topping_list
+
 ## TEST GUIDE: RUN  python test_menu_generate.py IN TERMINAL. IF THERE IS ASSERTIONERROR, THEN IT MEANS THE TEST DOESN'T PASS.
+
+## constraints to be tested
+## 1. the menu must contain exactly 10 different pizzas
+## 2. a pizza must be comprised of between 0 and 12 toppings, inclusive, using only the toppings listed above
+## 3. at least one vegetarian pizza must be offered
+## 4. a combination of pizza toppings cannot be duplicated at any point during the year
+## 5. on any given day, the Pizzeria staff should be able to generate the same menu multiple times (i.e. if they run out of all their menus, they should be able to print new ones)
+
+
+
 
 ## This file tests whether the generate_menu func could satisfy all the constraints or not
 ## I would loop for a whole year 2021 to test the first 3 constraints
@@ -15,13 +26,22 @@ def test_generate_menu():
         cur_date = (begin+datetime.timedelta(days=i)).strftime("%Y%m%d")
         menu = main(cur_date)
         assert len(menu)==10 ## constraint 1
-        assert has_vegan_pizza_in_menu(menu) ##constraint 2
+        assert has_vegan_pizza_in_menu(menu) ##constraint 3
         previous_year_cur_date = "2020"+cur_date[4:]
         previous_year_menu = main(previous_year_cur_date)
         for pizza in menu:
-            assert pizza not in topping_comb_list ##constraint 3
+            assert is_pizza_toppings_in_list(pizza) ## constraint 2
+            assert pizza not in topping_comb_list ##constraint 4
             topping_comb_list.append(pizza)
-            assert pizza in previous_year_menu ##constraint 4
+            assert pizza in previous_year_menu ##constraint 5
+    print("all tests passed")
+
+def is_pizza_toppings_in_list(pizza):
+    topping_list = get_topping_list()
+    for topping in pizza:
+        if topping not in topping_list:
+            return False
+    return True
     
 def has_vegan_pizza_in_menu(menu):
     for pizza in menu:
@@ -30,7 +50,7 @@ def has_vegan_pizza_in_menu(menu):
     return False
 
 def is_vegan_pizza(pizza):
-    non_vegan_toppings = ["ham","bacon","pepperoni"]
+    non_vegan_toppings = get_non_vegan_topping_list()
     for non_vegan_topping in non_vegan_toppings:
         if non_vegan_topping in pizza:
             return False
